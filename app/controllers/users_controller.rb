f2e642cebb03ad7a3c4559c1248a12a77999d703
bariczamonika <bariczamonika@gmail.com>
2019-06-08 10:47:35 +0000

    fully working user control for admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
     def index
-    @users = User.all
+    if current_user.admin?
+        @users = User.all
+    else
+        flash[:msg] = "You don't have access to this page"
+        redirect_to "/home/index"
+    end
     end
 end
