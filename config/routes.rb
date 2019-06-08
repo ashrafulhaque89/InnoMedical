94dac9e47b7b7f49602cd79f64c09374d9c50f7f
bariczamonika <bariczamonika@gmail.com>
2019-06-08 10:32:32 +0000

    added user management to admin

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   resources :patient_histories
   devise_for :users, controllers: { registrations: "user/registrations" } do
       resources :doctors
+      resources :users, only: [:index]
   end
   resources :doctors
   root :to => "home#index"
   get "home/index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users' => 'users#index'
 end
